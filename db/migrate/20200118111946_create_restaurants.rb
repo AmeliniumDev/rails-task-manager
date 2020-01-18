6f7fee8df790cada566c3a289ff03227b3a275a8
Amelia Urbanowicz <amelia.urbanowicz@gmail.com>
2020-01-18 11:32:25 +0000

    Add restaurant model and migrate db

---
+++ db/migrate/20200118111946_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :title
+      t.text :details
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+end
